a0baa9bef2292d48e93faaac044fe062f7294c6d
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2025-05-14 19:27:13 -0300

    fixing packages, creating repository/entities

---
+++ src/main/java/com/projetospringmicro/usuario/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,19 @@
+package com.projetospringmicro.usuario.infrastructure.repository;
+
+import com.estudos.projetospring.infrastructure.entity.Usuario;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    Void deleteByEmail(String email);
+}
