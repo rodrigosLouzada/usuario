221651a8a61b2b89df651b3b1aa1935934ed79f3
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2025-05-22 16:44:45 -0300

    metodo de comparacao de usuario na converter, metodo da service e criptografia

--- src/main/java/com/projetospringmicro/usuario/business/UsuarioService.java
+++ src/main/java/com/projetospringmicro/usuario/business/UsuarioService.java
@@ -6,6 +6,7 @@ import com.projetospringmicro.usuario.infrastructure.entity.Usuario;
 import com.projetospringmicro.usuario.infrastructure.exceptions.ConflictException;
 import com.projetospringmicro.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.projetospringmicro.usuario.infrastructure.repository.UsuarioRepository;
+import com.projetospringmicro.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     public UsuarioDTO salvarUsuario(UsuarioDTO usuarioDTO){
 
@@ -48,6 +50,19 @@ public class UsuarioService {
 
     public void deletaUsuarioPorEmail(String email){
         usuarioRepository.deleteByEmail(email);
+    }
+
+    public UsuarioDTO atualizarDadosUsuario(String token,UsuarioDTO dto){
+        String email = jwtUtil.extractUsername(token.substring(7));
+
+        dto.setSenha(dto.getSenha() != null ? passwordEncoder.encode(dto.getSenha()) : null);
+
+        Usuario usuarioEntity = usuarioRepository.findByEmail(email).
+                orElseThrow(() -> new ResourceNotFoundException("email não encontrado"));
+
+        Usuario usuario = usuarioConverter.updateUsuario(dto, usuarioEntity);
+
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
     }
 
 }
