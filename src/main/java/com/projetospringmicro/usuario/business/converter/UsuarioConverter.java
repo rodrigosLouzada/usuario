821c42199d57a395c91fa67124b918bb16cacee8
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2025-05-28 16:10:02 -0300

    na camada service, metodos de atualizacao de telefone/endereco, e metodos no convertor

--- src/main/java/com/projetospringmicro/usuario/business/converter/UsuarioConverter.java
+++ src/main/java/com/projetospringmicro/usuario/business/converter/UsuarioConverter.java
@@ -121,4 +121,26 @@ public class UsuarioConverter {
                 .ddd(telefoneDTO.getDdd() !=  null ? telefoneDTO.getDdd() : telefone.getDdd())
                 .build();
     }
+
+    public Endereco paraEnderecoEntity(EnderecoDTO enderecoDTO, Long idUsuario){
+
+        return Endereco.builder()
+                .rua(enderecoDTO.getRua())
+                .cidade(enderecoDTO.getCidade())
+                .cep(enderecoDTO.getCep())
+                .complemento(enderecoDTO.getComplemento())
+                .estado(enderecoDTO.getEstado())
+                .numero(enderecoDTO.getNumero())
+                .usuario_id(idUsuario)
+                .build();
+    }
+
+    public Telefone paraTelefoneEntity(TelefoneDTO telefoneDTO, Long idUsuario){
+        return Telefone.builder()
+                .numero(telefoneDTO.getNumero())
+                .ddd(telefoneDTO.getDdd())
+                .usuario_id(idUsuario)
+                .build();
+    }
+
 }
